a0c277130ae4412bc01751511d53013d2700a2d6
Fuyin Che <chefuyin@outlook.com>
2025-06-18 14:26:02 -0400

    build dashboard page and create sample chart, add about and download page component

--- dashboard-front/src/app/app.component.ts
+++ dashboard-front/src/app/app.component.ts
@@ -3,6 +3,11 @@ import {RouterLink, RouterOutlet} from '@angular/router';
 import {FooterComponent} from './footer/footer.component';
 import {NavComponent} from './nav/nav.component';
 import {HeaderComponent} from './header/header.component';
+// @ts-ignore
+import { NgChartsModule } from 'ng2-charts';
+import { ChartConfiguration, ChartType } from 'chart.js';
+import {DashboardComponent} from './dashboard/dashboard/dashboard.component';
+
 
 @Component({
   selector: 'app-root',
