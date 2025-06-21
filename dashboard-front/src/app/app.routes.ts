a38d5ace70919d2689513b9e45286e0d877127ee
Fuyin Che <chefuyin@outlook.com>
2025-06-21 15:24:58 +0800

    update about component

--- dashboard-front/src/app/app.routes.ts
+++ dashboard-front/src/app/app.routes.ts
@@ -6,11 +6,13 @@ import {UserLoginComponent} from './user-login/user-login.component';
 import {EnvironmentalDataComponent} from './environmental-data/environmental-data.component';
 import {EnvironmentalSampleDataComponent} from './environmental-sample-data/environmental-sample-data.component';
 import {DashboardComponent} from './dashboard/dashboard/dashboard.component';
+import {AboutComponent} from './about/about.component';
 
 export const routes: Routes = [
   {path:'environmentalData', component:EnvironmentalDataComponent,},
   {path:'environmentalSampleData', component:EnvironmentalSampleDataComponent,},
   {path:'dashboard', component:DashboardComponent,},
+  {path:'about', component:AboutComponent,},
 
   {path: '', redirectTo:'/environmentalSampleData', pathMatch:'full'},
   {path:'register', component: UserRegistrationComponent},
