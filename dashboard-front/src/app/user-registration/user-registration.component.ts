c09d7182922a3442a8da50d6882356f1f3fb6b47
Fuyin Che <chefuyin@outlook.com>
2025-06-23 13:57:51 +0800

    add user login and register pages

--- dashboard-front/src/app/user-registration/user-registration.component.ts
+++ dashboard-front/src/app/user-registration/user-registration.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpClient} from '@angular/common/http';
-import {API_BASE_URL} from '../../api.config';
-
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { API_BASE_URL } from '../../api.config';
+import {RouterOutlet} from '@angular/router';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-user-registration',
-  imports: [
-    FormsModule
-  ],
+  standalone: true,
+  imports: [CommonModule,
+        FormsModule,
+        RouterOutlet],
+  // templateUrl: './user-registration.component.html',
   templateUrl: './user-registration.component.html',
-  styleUrl: './user-registration.component.css'
+  styleUrls: ['./user-registration.component.css']
 })
 export class UserRegistrationComponent {
   user = {
@@ -19,17 +22,19 @@ export class UserRegistrationComponent {
     password: ''
   };
 
+  emailPrefix: string = '';
+
   constructor(private http: HttpClient) {}
 
   register() {
-    this.http.post('' +
-      '' +
-      `${API_BASE_URL}/register/`, this.user)
-      .subscribe(response => {
-        alert('User registered successfully!');
-      }, error => {
+    this.user.email = `${this.emailPrefix}@trentu.ca`;
+
+    this.http.post(`${API_BASE_URL}/register/`, this.user).subscribe({
+      next: () => alert('User registered successfully!'),
+      error: (err) => {
         alert('Registration failed');
-        console.error(error);
-      });
+        console.error('Error:', err);
+      }
+    });
   }
 }
